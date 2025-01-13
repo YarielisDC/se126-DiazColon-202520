26a1b16d3a122aee8f533f4ca61ed18c69eeed79
YarielisDC <ydiazcolon@email.neit.edu>
2025-01-13 11:22:40 -0500

    In class lab

---
+++ week2/W2D1_Class_Lab.py
@@ -0,0 +1,13 @@
+#Yarielis Diaz Colon
+#SE116.02
+#W2D1 in Class lab
+#1/13/2025
+
+#Program Prompt: 
+
+#--------------------Imports------------------------
+import csv
+
+
+#Connetced to file 
+with open ("week2\classLab2.csv") as csvfile:
