079d19fcfe70b64223291aae28f3773cd07c664e
YarielisDC <ydiazcolon@email.neit.edu>
2025-01-12 23:46:45 -0500

    Update w1L1.py

--- week1/w1L1.py
+++ week1/w1L1.py
@@ -3,20 +3,26 @@
 #W1D2 Lab 1 
 #1-7-2025 
 
-#Program Prompt - A program that will determine if a meeting one is to full, if the number of peole is less than o
+#Program Prompt - A program that will determine if a meeting room is to full, if the number of peole is less than o
 
 #Variable Dictionary 
 #people -  the amount of people attending the room
 #max_cap - the amount people allowed in the rom 
-
+#response - User's response if they would like to continue or not 
 #Imports
 
 #Functions
 def difference(people, max_cap):
-    '''Determines the number of people attending the meeting as wellas the the room capacity and return the difference. '''
+    '''Determines the number of people attending the meeting as well as  the room capacity and return the difference. '''
     diff = people - max_cap
     print()
     return diff
+def decision(response):
+    '''The Users's respoonse to whether or not they would like to continue in the program and enter another meeting's information'''
+    while response != 'y' and response != 'n':
+        respopnse = input("Invalid input. Please enter'y' for yes or 'n' for no").lower()
+    return response
+
 
 
 
@@ -27,3 +33,5 @@ people = int(input("How many people are attending? "))
 max_cap = int(input("What is the room capacity? "))
 
 difference(people, max_cap)
+
+response = input("Would you like to continue? 'y' for yes 'n' for no: ").lower()
