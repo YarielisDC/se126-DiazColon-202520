4e5ca964fa180a37d01c38016f9e480c2a228ec8
YarielisDC <ydiazcolon@email.neit.edu>
2025-01-27 22:22:23 -0500

    IN class lab

--- week4/W4d1_CL.py
+++ week4/W4d1_CL.py
@@ -1,13 +1,12 @@
-#W4D1 - Sequential Search
+#W4D1 - Sequential Search 
 
+#PROGRAM PROMPT: We will continue to work with the class_grades.csv file, as used in the W3D2 demo. We will practice connecting to a file, storing the file data into parallel lists, and creating new data for each student record based on these lists. We will then build a sequential search program which will allow us to find students in the file, and write data regarding them to a newly created file in our repository.
 
+#this program uses the file: class_grades.csv
 
-
-
-#------IMPORTS---------------------------------------------------
+#--IMPORTS---------------------------------------------------------
 import csv
-
-#------FUNCTIONS-----------------------------------------------
+#--FUNCTIONS-------------------------------------------------------
 def letter(num):
     if num >= 90:
         let = "A"
@@ -18,108 +17,176 @@ def letter(num):
     elif num >= 60:
         let = "D"
     elif num < 60:
-        let = "F"
+        let ="F"
     else:
-        let = "EROR"
-    return let #'let' value replaces the function call in hte main executing code
+        let = "ERROR"
 
-#---MAIN EXECLUDING CODE------------------------------------------
+    return let #the 'let' value will literally replace the letter() call in main code
+#--MAIN EXECUTING CODE---------------------------------------------
 
-#create empty lists to hold the file data
-fName = []
-lName = []
+#create some empty lists - one list for every potential field in the file
+firstName = []
+lastName = []
 test1 = []
 test2 = []
 test3 = []
 
+#connecting to the file --------
 with open("week4/class_grades.csv") as csvfile:
     file = csv.reader(csvfile)
 
-    for rec in file: 
-        #append the file data into appropiate lists
-        fName.append(rec[0])
-        lName.append(rec[1])
+    for rec in file:
+        #store data from current record to corresponding lists (each field is its own!)
+        #.append() --> adds the data to the next available space in the list (end)
+
+        #parallel lists --> data dispersed across lists, connected by the same index
+        firstName.append(rec[0])
+        lastName.append(rec[1])
         test1.append(int(rec[2]))
         test2.append(int(rec[3]))
         test3.append(int(rec[4]))
-#disconnected from file -- can still access the stored data via the lists
+#disconnected from file -------------------------------------------
 
-#process the list data to calc an avg score for each student, and file a letter grade equivalent 
+#process the lists to create and store each student's numeric average as well as letter grade average, then display all data back to the user
+num_avg = []        #holds student's numeric avg: (test1 + test2 + test3) / 3
+let_avg = []        #holds student's letter avg: letter(num_avg) return
 
-num_avg = [] #will hold each student's numeric average of test scores
-let_avg = [] #will hold each student's letter average of test schores
+for i in range(0, len(firstName)):
+    a = (test1[i] + test2[i] + test3[i]) / 3
+    num_avg.append(a)
+    let_avg.append(letter(a))
 
-for i in range(0, len(fName)):
-    a = (test1[i] + test2[i]+ test3[i]) / 3
-    #add average to num_avg list
-    num_avg.append(a) #can also do: num_avg.append((test[i] + test2[i] + test3[i] / 3 ))
+#print field headers for display below
+print(f"{'FIRST':10}  {'LAST':10}  {'T1':3}  {'T2':3}  {'T3':3}  {'# AVG':6}  {'L AVG'}")
+print("-----------------------------------------------------------------------------")
+#processing through lists for display
+for i in range(0, len(firstName)):
+    print(f"{firstName[i]:10}  {lastName[i]:10}  {test1[i]:3}  {test2[i]:3}  {test3[i]:3}  {num_avg[i]:6.1f}  {let_avg[i]}")
+print("-----------------------------------------------------------------------------")
+print(f"TOTAL STUDENTS IN FILE: {len(firstName)}")
 
-    l = letter(a) #return value to the letter() stored to 1
-    let_avg.append(l) #can also do: let_avg.append(letter(a))
+#SEQUENTIAL SEARCH-------------------------------------------------------------------
+#sequential search: to search for an item by comparing it to all items found within a collection (list)
+#        Searchnig Steps:
+#        1. Set Up & Gaining Search Item from user
+#        2. Perform the searching algorithm
+#        3. Filter and display results: tell user if the item is found or not; if found, display required data
 
-#process the lists to display all student data to the user
-print(f"{'FNAME':10}  {'LNAME':10}  {'T1':3}  {'T2':3} {'T3':3}  {'# AVG':6}  {'L AVG'}")
-print("-----------------------------------------------------------------------------------")
-for i in range(0, len(fName)):
-    print(f"{fName[i]:10}  {lName[i]:10}  {test1[i]:3}  {test2[i]:3}  {test3[i]:3}  {num_avg[i]:6.2f} {let_avg[i]}")
-print("-------------------------------------------------------------------------------------")
-print(f"There are {len(fName)} students in the file. ")
 
-#Write a progtam that allows a user to repeatedy search for a student 
+'''
+A simple overview of sequential search:
 
-print("\n\nWelcme to the Student Search Program\n\n")
+#step 1: set up and gain search
+found = -1 #we are using -1 because it is not a valid index location (technically); you could also use found = "x"
 
-answer = input("Would you like to begn searching? [y/n]: ").lower
+search = input("What are you searching for? ")
+
+#step 2: perform the searching algorithm
+for i in range(0, len(listYouAreSearchingThrough)):
+
+    if search == listYouAreSearchingThrough[i]: 
+        found = i #found drops its original value (-1 or "x") and is replaced with the current index location
+        #since this occurs in the if statement, it will only happen IF what we are looking for matches one of the values on the list we are searching through
+
+#step 3: check and display your results
+if found != -1: #(or "x" if that was your initial value
+    #found has changed, meaning at some point it checked into the if statement within our searching for loop
+    
+    print(f"Your search for {search} has been FOUND!")
+    #display relevant data/results here using 'found' as your index representer 
+    print(f"{listYouAreSearchingThrough[found]}")
+    
+else:
+    #found is still its initial value, meaning we did not find the data the user was looking for
+    #tell the user their search came up empty
+    print(f"Your search for {search} has NOT BEEN FOUND!")
+
+'''
+
+
+#build a repeatable program that allows a user to search through student records by first choosing a search type from a menu, performing the search based on the type, and displaying results
+print("\tWelcome to the Student Search Program")
+
+answer = input("Would you like to start your search? [y/n]: ").lower()
 
 while answer == "y":
-    
-    #get search type from user
-    print("\tSEARCH MENU OPTIONS")
-    print("1. Search by LAST name")
-    print("2. Search by LETTER GRADE")
+    #show user search menu 
+    print("\t~Search Menu~")
+    print("1. Search by LAST name")         #one search value found
+    print("2. Search by LETTER grade")      #multiple search values found
     print("3. EXIT")
-    
-    search_type = input("enter your search type")
-    if search_type == "1":
-        print("SEARCH BY LAST NAME")
-        
-        #get search item from user
-        found = -1 #invalid index number, will use to check later to see if a student has been found
-        
-        #get search from user
-        search_name = input("Enter the LAST NAME of the student you are searching for")
+    #gain search type 
+    search_type = input("Enter your search type [1-3]: ")
 
-        #perfrom search
-        for i in range(0, len(lName)):
-            #the FOR LOOP allows for the "sequence" part -> from beginning to end
-                if search_name.lower() == lName[i].lower():
-                    #the IF STATEMENT allows for the "search" part
-                    found = i #make found the current index, can be used later to display 
-      
-    elif search_type == "2":
-        print("\tSEARCH BY LETTER GRADE")
-        found = []
-        search_grade = input("Enter th LETTER GRADE of the students you are searching for: ")
+    #filter search options based on type
+    if search_type == "1": #LAST NAME
+        #sequential search - search for a student by their LAST name
+        #this version of sequential search is looking for ONE item, a specific and unique LAST name
+        
+        print("\tLAST NAME SEARCH~")
+        #step 1: set-up and gain search query
+        found = -1  #flag var, will be replaced with index position if name is found; we are using a -1 because it is not a valid index location
+        search_last = input("Enter the last name you wish to find: ") #name we are looking for
+
+        #step 2: perform search algo (seq. search -> for loop w/ if statement)
+        for i in range(0, len(lastName)):
+            #for loop performs the SEQUENCE - from start through end of list items
+
+            if search_last.lower() == lastName[i].lower(): 
+                #if performs the SEARCH - is what we're looking for here in the list?
+                found = i  #stores found item's INDEX LOCATION
+
+        #step 3: display results to user; make sure you give info: both for found or NOT found
+        if found != -1:
+            #last name FOUND!
+            print(f"Your search for {search_last} was FOUND! Here is their data: ")
+            print(f"{firstName[found]:10}  {lastName[found]:10}  {test1[found]:3}  {test2[found]:3}  {test3[found]:3}  {num_avg[found]:6.1f}  {let_avg[found]}")
+        else: 
+            #NOT found
+            print(f"Your search for {search_last} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+    
+    elif search_type == "2": #LETTER GRADE
+        print("\tLETTER GRADE SEARCH")
+
+        #sequential search - search for a collection of students based on their Letter Grade Average
+        #this version of sequential search is looking for MULTIPLE items, based on a specific letter grade
+
+        #step 1: set-up and gain search query
+        found = []  #empty list, found locations (index) will be stored if/when found
+        search_let= input("Enter the LETTER GRADE you wish to find: ") #grade we are looking through all students for
+
+        #step 2: perform search algo (seq. search -> for loop w/ if statement)
         for i in range(0, len(let_avg)):
-            if search_grade.upper() == let_avg[i]:
-                found.append() #add the current index (found), can be used later to display 
+            #for loop performs the SEQUENCE - from start through end of list items
 
-    elif search_type == "3":
-        print("SEARCH BY LETTER GRADE")
-    #display results
-    if not found: #the list is empty
+            if search_let.upper() == let_avg[i]: 
+                #if performs the SEARCH - is what we're looking for here in the list?
+                found.append(i)  #stores found item's INDEX LOCATION to the found list because we may have multiple students whose letter grade fits the searched for grade
+                print(f"Found a {search_let} grade in INDEX {i}")
 
-        #last name has been found! we can display data 
-        print(f"Your search for {search_grade} was *NOT* FOUND!")
-        print(f"This is cAsE sEnSiTiVe program - check your spelling and casing and try again. ")
-      
+        #step 3: display results to user; make sure you give info: both for found or NOT found
+        if not found: #'if not found' means 'found' is an EMPTY LIST
+            #NOT found
+            print(f"Your search for {search_let} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+        else: 
+            #last name FOUND!
+            print(f"Your search for {search_let} was FOUND! Here is their data: ")
 
+            #'found' is a list populated with index locations - we loop through this list, and use found[i] (which again, holds an INDEX from our other searched-through list) to be recalled and used below
+            for i in range(0, len(found)):
+                print(f"{found[i]}:  {firstName[found[i]]:10}  {lastName[found[i]]:10}  {test1[found[i]]:3}  {test2[found[i]]:3}  {test3[found[i]]:3}  {num_avg[found[i]]:6.1f}  {let_avg[found[i]]}")
+    elif search_type == "3": #exit
+        print("\t~EXIT~")
+        answer = "x"
     else:
-        print(f"Your search for {search_grade} was FOUND!")
-        for i in range(0, len(found)):
-            print(f"{fName[found[i]]:10}  {lName[found[i]]:10}  {test1[found[i]]:3}  {test2[found[i]]:3}  {test3[found[i]]:3}  {num_avg[found[i]]:6.2f} {let_avg[found[i]]}")
+        print("\t!INVALID ENTRY!")
     
+    #build a way out of the loop - answer should be able to change value! 
+    if search_type == "1" or search_type == "2":
+        #when search_type == "3" the user has chosen to exit, and if they did not provide a 1, 2, or 3 to search_type then they will automatically be brought back through the loop to see the menu again
+        answer = input("Would you like to search again? [y/n]: ").lower()
 
-    #build a way out of the loop
-    if search_type == "1" or search_type == "2": 
-        answer = input("Would you like to search again? [y/n]").lower
+
+print("\nThanks for using the search program. Goodbye!\n")
--- week4/w4_Class_lab.py
+++ week4/w4_Class_lab.py
@@ -5,9 +5,23 @@
 
 #------- IMPORTS ---------
 import csv
-
 #------------------------
 
+#------------ FUNCTIONS ------------------------------
+def letter(num):
+    if num >= 90:
+        let = "A"
+    elif num >= 80:
+        let = "B"
+    elif num >= 70:
+        let = "C"
+    elif num >= 60:
+        let = "D"
+    elif num < 60:
+        let = "F"
+    else:
+        let = "EROR"
+    return let #'let' value replaces the function call in hte main executing code
 #------------ LISTS ----------------------------------
 fName = []
 lName = []
@@ -16,7 +30,7 @@ test2 = []
 test3 = []
 
 
-#------ MAIN EXECUTING CODE -----------------------------
+#------ -------- MAIN EXECUTING CODE -----------------------------
 with open("week4/class_grades-2.csv") as csvfile:
     file = csv.reader(csvfile)
     
@@ -26,3 +40,69 @@ with open("week4/class_grades-2.csv") as csvfile:
         test1.append(int(rec[2]))
         test2.append(int(rec[3]))
         test3.append(int(rec[4]))
+
+#-------------OUT OF THE FILE--------------------
+num_avg = [] # Making a list so it stores the data into the 'num_avg' list
+letter_avg = [] #Making a list storing the data of the avg grades to a letter grades. 
+
+for i in range(0,len(fName)):  
+    a = test1[i] + test2[i] + test3[i]/ 3 #does the math for averages 
+    num_avg.append(a) #stores the total avg into the num_avg list
+    
+    l = letter(a) # a variable for the function that also holds the data for the letter(a) function 
+    letter_avg.append(l) #then the results from l or the letter function is stored into let_avg list
+
+#The final displays 
+print(f"{'FNAME':10}  {'LNAME':10}  {'T1':3}  {'T2':3} {'T3':3}  {'# AVG':6}  {'L AVG'}")
+print("-----------------------------------------------------------------------------------")
+for i in range(0, len(fName)):
+    print(f"{fName[i]:10}  {lName[i]:10}  {test1[i]:3}  {test2[i]:3}  {test3[i]:3}  {num_avg[i]:6.2f} {letter_avg[i]}")
+print(f"There are {len(fName)} students in the file. ")
+
+#SEQUENTIAL SEARCH 
+
+print("\n\nWelcme to the Student Search Program\n\n")
+
+answer = input("Would you like to begn searching? [y/n]: ").lower()
+
+while answer == "y":
+    #SEARCH MENU LIST
+    print("\tSEARCH MENU OPTIONS")                   
+    print("1. Search by LAST name")                  
+    print("2. Search by LETTER GRADE")               
+    print("3. EXIT")                                 
+    
+    search_type = input("Enter your search type: ").lower()
+
+    if search_type == "1":
+        print("Last Name Search")
+        found = -1 
+        search_Lname = input("Enter the last name you would like to search: ")
+
+        for i in range(0, len(lName)):
+            if search_Lname.lower() == lName[i].lower():
+                found = i 
+        if found != -1:
+            #last name FOUND!
+            print(f"Your search for {search_Lname} was FOUND! Here is their data: ")
+            print(f"{fName[found]:10}  {lName[found]:10}  {test1[found]:3}  {test2[found]:3}  {test3[found]:3}  {num_avg[found]:6.1f}  {letter_avg[found]}")
+        else: 
+            #NOT found
+            print(f"Your search for {search_Lname} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+    
+    elif search_type == "2":
+        print("\nLetter Grades")
+        
+        search_LetG = input("Enter the letter Grade you would like to search: ")
+        for i in range(0, len(letter_avg)):
+            if search_LetG == letter_avg[i]:
+                found.append(i)
+
+
+
+
+    elif search_type == "3":
+        print("EXIT")
+        answer = "o"
+        print("Goodbye!")
